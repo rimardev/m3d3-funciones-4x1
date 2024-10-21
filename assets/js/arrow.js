18a59afe4aec354d96fec532be268b0d1997e241
rimardev <catamarce.bs@gmail.com>
2024-10-21 17:02:56 -0300

    agrego solucion a los puntos 1 y 2 del desafio

--- assets/js/arrow.js
+++ assets/js/arrow.js
@@ -4,3 +4,6 @@ suma = function(a, b){
 return a + b
 }
  */
+const suma = (a, b) => a + b;
+
+suma(4, 4);
--- assets/js/funcion.js
+++ assets/js/funcion.js
@@ -4,3 +4,9 @@ function example(a, b, c){
 return a+b+c
 }
  */
+const suma = function (a, b, c) {
+  return a + b + c;
+  
+};
+
+suma(1, 2, 3)

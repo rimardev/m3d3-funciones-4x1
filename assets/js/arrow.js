5d344a1c3aa05f5ebaaac41e605fdb03bdf3c333
rimardev <catamarce.bs@gmail.com>
2024-10-21 10:14:28 -0300

    version inicial

---
+++ assets/js/arrow.js
@@ -0,0 +1,6 @@
+// Transforma la siguiente función a una arrow function de una línea en codigo funcional.
+/*
+suma = function(a, b){
+return a + b
+}
+ */
---
+++ assets/js/funcion.js
@@ -0,0 +1,6 @@
+// Transforma esta declaración de función a una función de expresión, en un codigo funcional.
+/*
+function example(a, b, c){
+return a+b+c
+}
+ */

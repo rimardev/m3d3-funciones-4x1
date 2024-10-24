2cca95fefba14b70e72ca3888e0fcf53390964db
rimardev <catamarce.bs@gmail.com>
2024-10-24 16:38:07 -0300

    agrego 4/4 del desafio ok.

--- assets/js/4colores.js
+++ assets/js/4colores.js
@@ -20,3 +20,35 @@ box1.addEventListener("click", () => changeBlack("box1"));
 box2.addEventListener("click", () => changeBlack("box2"));
 box3.addEventListener("click", () => changeBlack("box3"));
 box4.addEventListener("click", () => changeBlack("box4"));
+
+// ------------------------------------------------------
+
+let key0 = document.getElementById("key0");
+document.addEventListener("keydown", function(event){
+  console.log(event.key);
+  if (event.key === "a" || event.key === "A"){
+    key0.style.backgroundColor = "pink"; 
+  } else if (event.key === "s" || event.key === "S"){
+    key0.style.backgroundColor = "orange";
+  } else if (event.key === "d" || event.key === "D"){
+    key0.style.backgroundColor = "lightblue";
+  };
+});
+
+let key1 = document.getElementById("key1");
+document.addEventListener("keydown", function(event){
+  console.log(event.key);
+  if (event.key === "q" || event.key === "Q"){
+    key1.style.border = "1px solid gray";
+    key1.style.borderRadius = "10px";
+    key1.style.backgroundColor = "yellow";
+  } else if (event.key === "w" || event.key === "W"){
+    key1.style.border = "1px solid gray";
+    key1.style.borderRadius = "10px";
+    key1.style.backgroundColor = "royalblue";
+  } else if (event.key === "e" || event.key === "E"){
+    key1.style.border = "1px solid gray";
+    key1.style.borderRadius = "10px";
+    key1.style.backgroundColor = "brown";
+  };
+});
